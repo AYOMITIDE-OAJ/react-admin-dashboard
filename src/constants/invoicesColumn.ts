409aa3ec2073e59ded8b7d22a3a3b1d27a95fef6
AYOMITIDE OAJ <36337827+AYOMITIDE-OAJ@users.noreply.github.com>
2023-09-02 14:41:04 +0100

    setup necessary dependencies for chartjs

---
+++ src/constants/invoicesColumn.ts
@@ -0,0 +1,49 @@
+import { Typography } from "@mui/material";
+
+interface InvoicesColumnsProps {
+  colors: {
+    greenAccent: {
+      500: string;
+    };
+  };
+}
+
+const invoicesColumns = ({ colors }: InvoicesColumnsProps) => {
+  return [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: ({ row: { cost } }: { row: { cost: number } }) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${cost}
+        </Typography>
+    
+      ),
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
+  ];
+};
+
+export default invoicesColumns
--- src/constants/sidebarMenu.ts
+++ src/constants/sidebarMenu.ts
@@ -1,4 +1,14 @@
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 
 export const sidebarMenu = [
@@ -10,5 +20,63 @@ export const sidebarMenu = [
     {
         title: 'Info',
         tag: 'divider'
+    },
+    {
+        title: 'Manage Team',
+        icon: PeopleOutlinedIcon,
+        path: '/team',
+    },
+    {
+        title: 'Contacts Information',
+        icon: ContactsOutlinedIcon,
+        path: '/contacts',
+    },
+    {
+        title: 'Invoices Balances',
+        icon: ReceiptOutlinedIcon,
+        path: '/invoices',
+    },
+    {
+        title: 'Inputs',
+        tag: 'divider'
+    },
+    {
+        title: 'Profile Form',
+        icon: PersonOutlinedIcon,
+        path: '/form',
+    },
+    {
+        title: 'Calendar',
+        icon: CalendarTodayOutlinedIcon,
+        path: '/calendar',
+    },
+    {
+        title: 'FAQ Page',
+        icon: HelpOutlineOutlinedIcon,
+        path: '/faq',
+    },
+    {
+        title: 'Charts',
+        tag: 'divider'
+    },
+    {
+        title: 'Bar Chart',
+        icon: BarChartOutlinedIcon,
+        path: '/bar',
+    },
+    {
+        title: 'Pie Chart',
+        icon: PieChartOutlineOutlinedIcon,
+        path: '/pie',
+    },
+    {
+        title: 'Line Chart',
+        icon: TimelineOutlinedIcon,
+        path: '/line',
+    },
+    {
+        title: 'Geography Chart',
+        icon: MapOutlinedIcon,
+        path: '/geography',
     },
 ]
